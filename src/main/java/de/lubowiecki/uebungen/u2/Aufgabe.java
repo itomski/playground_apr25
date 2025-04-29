0f8034ca53a67e4ef50968a1fa22a7f98b0a94d1
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-04-29 14:17:26 +0200

    Tag 2

---
+++ src/main/java/de/lubowiecki/uebungen/u2/Aufgabe.java
@@ -0,0 +1,16 @@
+package de.lubowiecki.uebungen.u2;
+
+public class Aufgabe {
+
+    /* Schreibe eine Klasse Fahrzeug mit den Eigenschaften
+        Kennzeichen, Marke, Modell und Baujahr.
+        Erweitere diese Klasse durch eine Klasse PKW, die 4 Insassen aufnehmen kann
+        und eine Klasse LKW, die 8 Zuladungen aufnehmen kann.
+        Erweitere diese Klasse PKW durch eine Klasse SUV, die neben den Innsassen
+        auch 2 Zuladungen aufnehmen kann.
+
+        Probiere es mit Beispieldaten aus.
+        Implementiere jeweils die toString-Methode so, dass eine vernünftige Ausgabe erfolgen kann.
+     */
+
+}
