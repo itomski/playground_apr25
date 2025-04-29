2b52e031cdf58a0eb38689f30e92981d2a12e313
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-04-29 11:19:06 +0200

    Tag 2

--- src/main/java/de/lubowiecki/uebungen/u1/Aufgabe.java
+++ src/main/java/de/lubowiecki/uebungen/u1/Aufgabe.java
@@ -6,6 +6,7 @@ public class Aufgabe {
     Schreibe eine Klasse für die Verwaltung eines Campingplatzes.
     Auf diesem Platz gibt es 10 Locations.
     Die Location-Varianten sind: Zelt, Wohnwagen, Mobilhome
+    (Mögliche Attribute: Nr, Art, Tagespreis)
     Alle Varianten sollen in einer Datenstruktur verwaltet werden.
     Über eine Schleife sollen alle Locations ausgegeben werden
     */
