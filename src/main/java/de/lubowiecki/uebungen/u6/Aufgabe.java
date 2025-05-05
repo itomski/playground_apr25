a7567fcbbd371e71975ec268a7220ac37d2dc302
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-05 11:57:23 +0200

    Tag 5

--- src/main/java/de/lubowiecki/uebungen/u6/Aufgabe.java
+++ src/main/java/de/lubowiecki/uebungen/u6/Aufgabe.java
@@ -2,7 +2,7 @@ package de.lubowiecki.uebungen.u6;
 
 public class Aufgabe {
 
-    // Implementiere mehrere (mind. 3) Vertraucher (d.h. Klassen, die das Interace erfüllen)
+    // Implementiere mehrere (mind. 3) Vertraucher (d.h. Klassen, die das Interface erfüllen)
     // z.B. Lampe, Computer, Ventilator
 
 }
