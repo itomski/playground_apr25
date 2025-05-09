cfae518e4494d19ad210d457ab0cd9c43bd05bb8
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-09 10:41:32 +0200

    Aufgaben

---
+++ src/main/java/de/lubowiecki/uebungen/u8/Aufgabe.java
@@ -0,0 +1,32 @@
+package de.lubowiecki.uebungen.u8;
+
+public class Aufgabe {
+
+    /*
+    Aufgabe 1:
+    Schreibe eine Klasse, die einen Kursteilnehmer beschreibt.
+    Bei der Instanzierung der Klasse wird eine Teilnehmernummer vergeben.
+    Diese Nummer wird in einer Datei gespeichert, so dass beim nächten Programmaufruf
+    und Instanzierung die nachfolgende Nummer vergeben wird.
+     */
+
+    /*
+    Aufgabe 2:
+    Schreibe ein Programm für eine Einkaufsliste.
+    Über die Konsole oder Eigabefeld werden die Einträge abgefragt.
+    Beim Verlassen des Programms wird die Liste serialisiert und in eine Datei gespeicht.
+    Beim Programmstart sind die vorherigen Einträge wieder vorhanden.
+    Das ganze kann als Konsolen-Programm oder über eine GUI (JavaFX) gelöst werden
+     */
+
+    /*
+    Aufgabe 3:
+    Schreibe eine Datei mit 5 Informationen, die mit einem Komma getrennt sind.
+    Beim Start des Programms werden diese 5 Informationen eingelesen und in einem Array von Strings
+    bereitgestellt.
+
+    5 Informationen: Name der App, Kurzbeschreibung, Adminuser, Adminpassword, Sprache
+    Beispiel: Todolist, Eine Liste mit Todods, tlubo, geheim, deutsch
+     */
+
+}
