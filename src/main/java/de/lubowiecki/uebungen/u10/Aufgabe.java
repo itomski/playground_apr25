6794dbf6991425b4a40e18bc0802cb98717ba70d
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-13 11:28:47 +0200

    Tag 10

---
+++ src/main/java/de/lubowiecki/uebungen/u10/Aufgabe.java
@@ -0,0 +1,19 @@
+package de.lubowiecki.uebungen.u10;
+
+public class Aufgabe {
+
+    /*
+    Schreibe ein Programm für die Verwaltung von Kunden.
+    Die Personen sollen kunden sollen eine Kundenummer, Vorname, Nachname und Geburtsdatum enthalten
+    Schaffe die Möglichkeit die Kundenliste zu sortieren.
+    Natürliche Ordnung erfolgt nach Kundennummer.
+    Ermögliche auch die Sortiereung nach Vorname, Nachname und Geburtsdatum.
+    */
+
+    /*
+    Schreibe ein Programm für die Verwaltung von Fahrzeugen.
+    Jedes Fahrzeug hat ein Kennzeichen (und andere Eigenschaften)
+    Pro Kennzeichen darf es nur 1 Objekt in der Sammlung geben.
+    Verwende dafür ein Set.
+    */
+}
