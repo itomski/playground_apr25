ffd4f5f4cc6e4d1264a2af51ec14a77a20b71c37
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-19 14:04:33 +0200

    Aufgaben

---
+++ src/main/java/de/lubowiecki/uebungen/u11/Aufgabe.java
@@ -0,0 +1,30 @@
+package de.lubowiecki.uebungen.u11;
+
+public class Aufgabe {
+
+    /*
+    Aufgabe 1
+    Schreibe eine einfache App für eine Büchersammlung
+    Dazu gehört eine Datenbank mit dem Namen "archive"
+    Darin erzeuge eine Tabelle mit dem Namen "books"
+    Die Tabelle hat folgende Spalten
+        id, title, publication, author
+    publication soll das Erscheinungsjahr sein.
+
+    Leide einige Bücher darin ab und frage diese anschließend ab.
+     */
+
+    /*
+    Aufgabe 2
+    Schreibe eine App zu Verwaltung von Punktständen für Spiele
+    Darin soll für einen Namen ein Punktstand festgehalten werden.
+    Sollte der gleiche Spieler noch mal eingetragen werden, sollen seine alten
+    Punkte überschriben werden
+
+    Wähle dafür eine passende Collection.
+    Die App fragt den Usernamen und den Punktestand ab.
+    Das mehrmals nacheinander und anschließend zeigt sie alles an
+     */
+
+
+}
