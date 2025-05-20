fc78c6cfc39874e081c9a59e26ac2cd035724ac4
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-20 13:43:12 +0200

    Übung

---
+++ src/main/java/de/lubowiecki/uebungen/u12/Aufgabe.java
@@ -0,0 +1,19 @@
+package de.lubowiecki.uebungen.u12;
+
+public class Aufgabe {
+
+    /*
+    Löse die Übung 1 und 2 für Tag 23 (Lernplattform: Übungen)
+
+    Schreibe ein Datenbank Repository für eine Kundenverwaltung.
+    Die Datenbank soll "kundenverwaltung" heißen.
+    Die Tabelle soll "kunden" heißen.
+    Neben Vorname, Nachname und Geburtsdatum, soll für jeden Kunden auch eine Adresse gespeichert werden.
+    Überlege wie die Adresse gem. der Normalisierung abgelegt werden sollte.
+
+    Das Repository soll vorerst nur lesend auf die DB zugreifen.
+    Die Beispieldaten sollen direkt über phpMyAdmin eingegeben werden.
+     */
+
+
+}
