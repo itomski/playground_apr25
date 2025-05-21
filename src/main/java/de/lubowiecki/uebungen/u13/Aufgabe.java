12cf29a2a22532e9b1fa7bd1fb6823420ff0fbf4
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-21 11:30:02 +0200

    Repository

---
+++ src/main/java/de/lubowiecki/uebungen/u13/Aufgabe.java
@@ -0,0 +1,14 @@
+package de.lubowiecki.uebungen.u13;
+
+public class Aufgabe {
+
+    /*
+    Falls nicht vorhanden, importiert bitte die Tabelle der Kundenverwaltung (SQL-Script)
+    Danach...
+    Schreibe 3 SQL Anweisungen zum Einfügen von Datensätzen (Kunden)
+    Schreibe 3 SQL Anweisungen zum Abfragen von Datensätzen (z.B. nach Id, Vorname, Nachname)
+    Schreibe 3 SQL Anweisungen zum Updaten von Datensätzen
+    Schreibe 2 SQL Anweisungen zum Löschen von Datensätzen (z.B. nach Id, Nachname)
+     */
+
+}
