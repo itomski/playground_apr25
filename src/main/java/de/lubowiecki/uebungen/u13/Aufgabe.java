da4ac5a5a0beb151edcaea169547060bf0732ee9
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-21 13:26:42 +0200

    Repository

--- src/main/java/de/lubowiecki/uebungen/u13/Aufgabe.java
+++ src/main/java/de/lubowiecki/uebungen/u13/Aufgabe.java
@@ -3,6 +3,9 @@ package de.lubowiecki.uebungen.u13;
 public class Aufgabe {
 
     /*
+
+    Aufgabe 1
+
     Falls nicht vorhanden, importiert bitte die Tabelle der Kundenverwaltung (SQL-Script)
     Danach...
     Schreibe 3 SQL Anweisungen zum Einfügen von Datensätzen (Kunden)
@@ -10,6 +13,21 @@ public class Aufgabe {
     Schreibe 3 SQL Anweisungen zum Updaten von Datensätzen
     Schreibe 2 SQL Anweisungen zum Löschen von Datensätzen (z.B. nach Id, Nachname)
     Alle Anweisungen sollen über phpMyAdmin ausgeführt werden
+
+
+    Aufgabe 2
+    Schreibe eine Klasse mit 6 Methoden.
+    Jede der Methoden soll eine Exception (frei wählbar) produzieren.
+    3 Methoden behandeln die Exceptions direkt und 3 Methoden geben die Exceptions weiter
+    an den Aufrufer. Teste diese Methoden.
+    Was sind die Vor- bzw. Nachteile der jeweiligen Handhabung der Exceptions?
+
+
+    Aufgabe 3
+    Schreibe ein Programm für eine Task-Liste, die ihre Daten in der Datenbank (SQLite oed MySQL) speichert.
+    Tasks bestehen aus Titel, Beschreibung, Datum und Status (erledigt oder noch offen).
+    Du kannst dich für eine GUI (JavaFX) oder ein Konsolenprogramm entscheiden.
+
      */
 
 }
