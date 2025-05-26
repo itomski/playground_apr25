b7ee41852961cff5211da0b0fc00fbb1936f83cb
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-26 13:09:36 +0200

    Patterns

---
+++ src/main/java/de/lubowiecki/uebungen/u16/Aufgabe.java
@@ -0,0 +1,30 @@
+package de.lubowiecki.uebungen.u16;
+
+public class Aufgabe {
+
+    /*
+
+    Aufgabe 1:
+    Schreibe einen Adapter, der es zwei inkompatibilen Klassen eine zusammenarbeit ermöglicht.
+
+    Klasse: Saege
+    Methode: zuschneiden(Material material, double cm)
+
+    Interface Material
+    Methode: kuerzen(double cm)
+
+    Klassa WoodenBoard
+    Methode: cut(double inch)
+
+    Aktuell werden Objekte, die das Interface Material implementieren an Methode zuschneiden
+    der Saege übergeben. Dabei wird die Größe in cm angegeben.
+
+    WoodenBoard soll auch verwendbar sein, leider heißt die Methode des WoodenBoars cut und verwendet inch.
+    Das Problem soll durch einen Adapter gelöst werden
+
+
+     */
+
+
+
+}
