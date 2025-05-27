bb0151d98bb3027dbeb122e80b8a12a0e7b643a6
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-27 11:50:14 +0200

    u16

---
+++ src/main/java/de/lubowiecki/uebungen/u17/Aufgabe.java
@@ -0,0 +1,20 @@
+package de.lubowiecki.uebungen.u17;
+
+public class Aufgabe {
+
+    /*
+    Aufgabe 1
+    Entwerfe ein Anwendungsfall-Diagramm für die Umsetzung eines Online-Pizza-Service.
+
+    Aufgabe 2
+    Entwerfe ein Aktivitäts-Diagramm für Online-Pizza-Bestellung
+
+    Aufgabe 3
+    Entwerfe ein Klassendiagramm für Klassen, die für die Umsetzung des Online-Pizza-Service
+    aus deiner Sicht nötig wären
+
+    Aufgabe 4
+    Entwerfe ein Sequenz-Diagramm für die Bestellung
+     */
+
+}
