42cd6248de9c265a7f1b757c30ae52cbe21924e4
Tomasz Lubowiecki <tomasz@lubowiecki.de>
2025-05-28 12:44:08 +0200

    u17

---
+++ src/main/java/de/lubowiecki/uebungen/u18/Aufgabe.java
@@ -0,0 +1,15 @@
+package de.lubowiecki.uebungen.u18;
+
+public class Aufgabe {
+
+    /*
+    Aufgabe 1:
+    Baue ein JavaFX Taschenrechner Projekt mit einer Oberfläche auf basis von FXML.
+    Die Oberfläche soll ähnlich der Vorgabe sein (siehe rechner.png)
+    Verwende dazu CSS.
+
+    Aufgabe 2:
+    Implementiere die Möglichkeit über die Tasten 0-9 Zahlen in das Textfeld auszugeben.
+     */
+
+}
